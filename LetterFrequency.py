0e648d2e27f1e259950181a7aaad90b31c3fd14e
cjanes-uno <cjanes@unomaha.edu>
2025-02-23 22:40:21 +0000

    LetterFrequency done.

--- LetterFrequency.py
+++ LetterFrequency.py
@@ -15,10 +15,13 @@ def countLetters(message):
     freq = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 
     #loop through each letter
+    for ch in message:
     #Find the position in the alphabet
-    #Increase the frequency at that position. If position was 5, then frequencies[5] = frequencies[5] + 1
-
-
+        if ch in alpha:
+            index = alpha.index(ch)
+    #Increase the frequency at that position.
+    #If position was 5, then frequencies[5] = frequencies[5] + 1
+            freq[index] = freq[index] + 1
 
     #Create the output text in the format A,5\n if there were 5 letter A in the message.
     #Remember that the \n is the symbol for a new line.
